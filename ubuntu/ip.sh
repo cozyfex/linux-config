8c744513542f293e30bd6cd9a465307459f2d30a
Felix D.H. Kang <cozyfex@gmail.com>
2021-06-03 01:25:43 +0900

    mode

--- ubuntu/ip.sh
+++ ubuntu/ip.sh
@@ -1,5 +1,5 @@
-IP="192.168.56.10/24"
-GATEWAY="192.168.56.1"
+IP="10.0.2.10/24"
+GATEWAY="10.0.2.2"
 ETHERNET_NAME="enp0s3"
 
 while getopts "i:g:e:" opt; do
@@ -28,23 +28,6 @@ EOT
 
 echo "Change IP Apply"
 netplan apply
-HOSTNAME="master01"
-IP="192.168.56.10/24"
-GATEWAY="192.168.56.1"
-ETHERNET_NAME="enp0s3"
-
-while getopts "h:i:g:e:" opt; do
-  case "$opt" in
-  h) HOSTNAME="$OPTARG" ;;
-  i) IP="$OPTARG" ;;
-  g) GATEWAY="$OPTARG" ;;
-  e) ETHERNET_NAME="$OPTARG" ;;
-  ?) helpFunction ;; # Print helpFunction in case parameter is non-existent
-  esac
-done
-
-echo "Change Hostname"
-hostnamectl set-hostname ${HOSTNAME}
 
 echo "================================================"
 ip addr show ${ETHERNET_NAME}
